e15157e9a42cce6c7bde1a8703801d98fa0e145e
Uldis Zingis <uldis.zingis@makit.lv>
2016-10-26 10:49:28 -0600

    Setup buttons

--- ProgrammaticViewsWithAnimations/ViewController.swift
+++ ProgrammaticViewsWithAnimations/ViewController.swift
@@ -10,16 +10,47 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: - Properties
+
+    let topLeftButton = UIButton()
+    let topRightButton = UIButton()
+    let bottomLeftButton = UIButton()
+    let bottomRightButton = UIButton()
+
+    // MARK: - General
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        setupButton()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    // MARK: - Methods
+
+    func setupButton() {
+        let buttons = [topLeftButton, topRightButton, bottomLeftButton, bottomRightButton]
+        let colors = [UIColor.green, UIColor.yellow, UIColor.blue, UIColor.red]
+
+        var i = 0
+        for button in buttons {
+            button.backgroundColor = colors[i]
+            button.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+            button.addTarget(self, action: #selector(buttonExited), for: .touchDragExit)
+            view.addSubview(button)
+            i += 1
+        }
+    }
+    
+    func setupConstraints() {
+        view.translatesAutoresizingMaskIntoConstraints = false
+        
     }
 
+    func buttonTapped() {
+        
+    }
 
+    func buttonExited() {
+    
+    }
 }
 
